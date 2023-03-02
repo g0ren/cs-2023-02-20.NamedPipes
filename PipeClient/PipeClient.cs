9aa4d7b588c7de14a502ec847ff26629aaeb2afa
g0ren <g0ren@mail.ru>
2023-03-02 23:35:10 +0200

    Add project files.

---
+++ PipeClient/PipeClient.cs
@@ -0,0 +1,69 @@
+﻿using System.IO.Pipes;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NamedPipes
+{
+    class PipeClient
+    {
+        public const string pipeName = "MyPipe";
+
+        private static void SendMessage(string message, NamedPipeClientStream namedPipeClient)
+        {
+            if (!namedPipeClient.IsConnected)
+            {
+                namedPipeClient.Connect();
+            }
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            namedPipeClient.Write(messageBytes, 0, messageBytes.Length);
+        }
+
+        private static string ReceiveMessage(NamedPipeClientStream namedPipeClient)
+        {
+            if (!namedPipeClient.IsConnected)
+            {
+                namedPipeClient.Connect();
+            }
+            StringBuilder messageBuilder = new StringBuilder();
+            string messageChunk = string.Empty;
+            byte[] messageBuffer = new byte[5];
+            do
+            {
+                namedPipeClient.Read(messageBuffer, 0, messageBuffer.Length);
+                messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                messageBuilder.Append(messageChunk);
+                messageBuffer = new byte[messageBuffer.Length];
+            }
+            while (!namedPipeClient.IsMessageComplete);
+
+            return Regex.Replace(messageBuilder.ToString(), @"\0+", string.Empty);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to NamedPipe");
+            Console.WriteLine("Client commands are:");
+            Console.WriteLine("do - do something");
+            Console.WriteLine("cat - send me a picture of a cat");
+            Console.WriteLine("quit - quit");
+
+            NamedPipeClientStream namedPipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut);
+            string message = "";
+            while (message != "quit")
+            {
+                Console.WriteLine("Enter command");
+                message = Console.ReadLine();
+                SendMessage(message, namedPipeClient);
+                if (message == "quit")
+                {
+                    namedPipeClient.Close();
+                    break;
+                }
+                namedPipeClient.ReadMode = PipeTransmissionMode.Message;
+                Console.WriteLine($"Message from server:\n {ReceiveMessage(namedPipeClient)}");
+            }
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PipeServer/PipeServer.cs
@@ -0,0 +1,85 @@
+﻿using System.IO.Pipes;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NamedPipes
+{
+    class PipeServer
+    {
+        public const string pipeName = "MyPipe";
+
+        private static void SendMessage(string message, NamedPipeServerStream namedPipeServer)
+        {
+            if (!namedPipeServer.IsConnected)
+            {
+                namedPipeServer.WaitForConnection();
+            }
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            namedPipeServer.Write(messageBytes, 0, messageBytes.Length);
+        }
+
+        private static string ReceiveMessage(NamedPipeServerStream namedPipeServer)
+        {
+            if (!namedPipeServer.IsConnected)
+            {
+                namedPipeServer.WaitForConnection();
+            }
+            StringBuilder messageBuilder = new StringBuilder();
+            string messageChunk = string.Empty;
+            byte[] messageBuffer = new byte[5];
+            do
+            {
+                namedPipeServer.Read(messageBuffer, 0, messageBuffer.Length);
+                messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                messageBuilder.Append(messageChunk);
+                messageBuffer = new byte[messageBuffer.Length];
+            }
+            while (!namedPipeServer.IsMessageComplete);
+
+            return  Regex.Replace(messageBuilder.ToString(), @"\0+", string.Empty);
+        }
+
+        public static void Main()
+        {
+            NamedPipeServerStream namedPipeServer = new NamedPipeServerStream(pipeName, PipeDirection.InOut,
+                1, PipeTransmissionMode.Message);
+            string message = "";
+            while (message != "quit")
+            {
+                if (!namedPipeServer.IsConnected)
+                {
+                    try
+                    {
+                        namedPipeServer.WaitForConnection();
+                    }catch(System.IO.IOException e)
+                    {
+                        break;
+                    }
+                }
+                message = ReceiveMessage(namedPipeServer);
+
+                Console.WriteLine($"Received command: {message}");
+
+                switch (message)
+                { 
+                    case "do":
+                        SendMessage("Doing something...", namedPipeServer);
+                        break;
+                    case "cat":
+                        SendMessage($"   ____\r\n  (.   \\\r\n    \\  |  \r\n     \\ |___(\\--/)\r\n   __/    (  . . )\r\n  \"'._.    '-.O.'\r\n       '-.  \\ \"|\\\r\n          '.,,/'.,,mrf", namedPipeServer);
+                        break;
+                    case "quit": 
+                        namedPipeServer.Close(); 
+                        Console.WriteLine("Quit command received, quitting");
+                        break;
+                    default:
+                        SendMessage($"I've received command \'{message}\'. Not sure what to do with it...", namedPipeServer);
+                        break;
+                }
+            }
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
